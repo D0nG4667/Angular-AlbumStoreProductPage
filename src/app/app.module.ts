f9eb960f3d32839da96e36b79d42a4e05f07e900
D0nG4667 <osasumwen.okundaye@lifesci.uniben.edu>
2019-11-08 00:33:31 +0100

    Adding ProductService As A Provider

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,12 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { HttpModule } from "@angular/http";
 
-import { AppComponent } from './app.component';
-import { ProductPageComponent } from './product-page/product-page.component';
-import { ProductDescriptionComponent } from './product-description/product-description.component';
-import { ProductService } from './product.service';
+import { AppComponent } from "./app.component";
+import { ProductPageComponent } from "./product-page/product-page.component";
+import { ProductDescriptionComponent } from "./product-description/product-description.component";
+import { ProductService } from "./product.service";
 
 @NgModule({
   declarations: [
@@ -14,12 +14,8 @@ import { ProductService } from './product.service';
     ProductPageComponent,
     ProductDescriptionComponent
   ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule
-  ],
-  providers: [],
+  imports: [BrowserModule, FormsModule, HttpModule],
+  providers: [ProductService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
