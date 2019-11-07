36f2c3cb24162b6aa1e0d519a27cbcbac2b94b29
D0nG4667 <osasumwen.okundaye@lifesci.uniben.edu>
2019-11-08 00:38:40 +0100

    Importing Http And Response Into ProductService

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,8 +1,8 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
+import { Http } from "@angular/http";
+import { Response } from "@angular/http";
 
 @Injectable()
 export class ProductService {
-
-  constructor() { }
-
+  constructor() {}
 }
