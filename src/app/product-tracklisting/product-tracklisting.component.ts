131071f651d21855869906dca28172025bde066a
D0nG4667 <osasumwen.okundaye@lifesci.uniben.edu>
2019-11-08 21:29:43 +0100

    Importing The Album Interface Into The ProductTracklistingComponent

--- src/app/product-tracklisting/product-tracklisting.component.ts
+++ src/app/product-tracklisting/product-tracklisting.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+
+import { Album } from "../album";
 
 @Component({
-  selector: 'app-product-tracklisting',
-  templateUrl: './product-tracklisting.component.html',
-  styleUrls: ['./product-tracklisting.component.css']
+  selector: "app-product-tracklisting",
+  templateUrl: "./product-tracklisting.component.html",
+  styleUrls: ["./product-tracklisting.component.css"]
 })
 export class ProductTracklistingComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
