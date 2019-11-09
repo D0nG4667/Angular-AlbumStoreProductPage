ccc7fa9142120a6c5fc5f2734712e3355d3fc2be
D0nG4667 <osasumwen.okundaye@lifesci.uniben.edu>
2019-11-09 12:43:52 +0100

    Adding a getProducts Method To The ProductService

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -10,11 +10,14 @@ import { Product } from "./product";
 export class ProductService {
   private _albumUrl = "../assets/album.json";
   private _productsUrl = "../assets/products.json";
+
   getAlbum(id: number): Observable<Album> {
     return this._http
       .get(this._albumUrl)
       .map(response => <Album>response.json());
   }
+
   getProducts() {}
+
   constructor(private _http: Http) {}
 }
